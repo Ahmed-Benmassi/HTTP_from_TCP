a7ff7e4ddd5a98f76496c7a2fef9b113a1efb7d8
Ahmed-Benmassi <ahmedbenmassi@gmail.com>
2026-02-22 00:24:14 +0000

    blabla

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,45 +1,11 @@
 package main
 
 import (
-	"errors"
+	"httpfromtcp/internal/request"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
 )
-
-
-func getLinesChannel(f io.ReadCloser) <-chan string {        //func that take the message and read  it  8 byte and each end line with \n we split it and somme it to a line that is readabael
-	lines := make(chan string)                               
-	go func() {
-		
-		defer close(lines)
-		currentLineContents := ""
-		for {
-			b := make([]byte, 8)
-			n, err := f.Read(b)
-			if err != nil {
-				if currentLineContents != "" {
-					lines <- currentLineContents
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				return
-			}
-			str := string(b[:n]) 
-			parts := strings.Split(str, "\n")                    //go func take the 8 byte readed message and take the last line of the message and complete it to tha first line 
-			for i := 0; i < len(parts)-1; i++ {                  // the last part of the slice that is a beggining of the next line will be the first word of he next line 
-				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
-				currentLineContents = ""
-			}                                                       //example  "hello worltd go on" ist goes like this [hello wo,rld go on] 
-			currentLineContents += parts[len(parts)-1]
-		}
-	}()
-	return lines
-}
 
 
 
@@ -59,9 +25,16 @@ func main() {
 			log.Fatal("ERROR ","ERROR",err)
 		}
 
-		for line := range getLinesChannel(conn) {      //printing the message like network like file
-			fmt.Println(line)
+		r,err:=request.RequestFromReader(conn)
+		if err!=nil{
+			log.Fatal("ERROR ","ERROR",err)
 		}
+
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s\n",r.RequestLine.Method)		
+		fmt.Printf("- Target: %s\n",r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n",r.RequestLine.HttpVersion)
+
 		fmt.Printf("==============================================\r\n")
 		
 	
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module MODULE_NAME
+module httpfromtcp
 
 go 1.25.0
 
